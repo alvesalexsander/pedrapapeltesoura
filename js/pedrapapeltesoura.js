6b4dd2896d651106da89de39b475a8013b8dda34
sashaclimax <wolfiganlex15@hotmail.com>
2017-08-22 20:39:05 -0300

    implemantados emoticons e textos revisados.

--- js/pedrapapeltesoura.js
+++ js/pedrapapeltesoura.js
@@ -6,34 +6,34 @@ if (computerChoice < 0.34) {
 	computerChoice = "papel";
 } else {
 	computerChoice = "tesoura";
-} alert("Computer: " + computerChoice);
+} alert("Eu escolho: " + computerChoice);
 
 var compare = function(choice1, choice2){
     if (choice1 === choice2){
-        alert("Empate!");
+        alert("乁( ͠°_ °)ㄏ Empate!");
     }   
     else if (choice1 === "pedra"){
         if (choice2 ==="tesoura"){
-            alert("Pedra esmaga tesoura!");
+            alert("Você ganhou ᕕ(`益´)ᕗ! Pedra esmaga tesoura!");
         }
         else if (choice2 === "papel"){
-            alert("Papel embrulha pedra!");
+            alert("Você perdeu (∩☯ヘ☯)⊃━☆ﾟ.*... Papel embrulha pedra!");
         }
     }
     else if (choice1 === "tesoura"){
         if (choice2 === "papel"){
-            alert("Tesoura corta papel");
+            alert("Você ganhou ᕕ(`益´)ᕗ! Tesoura corta papel");
         }
         else if (choice2 === "pedra"){
-            alert("Pedra esmaga tesoura!");
+            alert("Você perdeu (∩☯ヘ☯)⊃━☆ﾟ.*... Pedra esmaga tesoura!");
         }
     }
     else if (choice1 === "papel"){
         if (choice2 === "pedra"){
-            alert("Papel embrulha pedra!");
+            alert("Você ganhou ᕕ(`益´)ᕗ! Papel embrulha pedra!");
         }
         else if (choice2 === "tesoura"){
-            alert("Tesoura corta papel!");
+            alert("Você perdeu (∩☯ヘ☯)⊃━☆ﾟ.*... Tesoura corta papel!");
         }
     }
 };
